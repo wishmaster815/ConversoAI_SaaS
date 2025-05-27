e4cde22c0ac5b4d353dd801eb9f2f3a91e2637e8
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-05-27 21:22:20 +0530

    Implement Navigation Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Navbar from '@/components/Navbar'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
     <>
-    
     <h1 className='text-2xl underline ' >Welcome to LMS SaaS application</h1>
     <Button>Click me</Button>
+
     </>
   )
 }
