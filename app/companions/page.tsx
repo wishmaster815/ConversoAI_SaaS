c07325b2c76f123df6146ea6fa3367dd5d278b4e
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-06-01 23:44:53 +0530

    add Companion Library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,38 @@
-const companionslibrary = () => {
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import CompanionCard from "@/components/Card";
+import { getSubjectColor } from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+import { SearchParams } from "@/types";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return (
-    <div>companionslibrary</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
   )
 }
 
-export default companionslibrary
+export default CompanionsLibrary
