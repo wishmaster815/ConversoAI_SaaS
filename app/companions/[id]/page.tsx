bd2364d6558946113160fcbfb1e9f01d236752aa
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-06-03 00:30:58 +0530

    Vapi Setup and Header

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { getCompanion } from "@/lib/actions/companion.actions"
+import { getSubjectColor } from "@/lib/utils"
+import { currentUser } from "@clerk/nextjs/server"
+import Image from "next/image"
+import { redirect } from "next/navigation"
 
-const companionSession = () => {
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>
+}
+
+// params = /url/{id}
+// searchParams = /url?key=value1&key1=value2 
+
+const companionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params
+  const { name, subject, title, topic, duration } = await getCompanion(id)
+  const user = await currentUser()
+
+  if (!user) redirect('/sign-in');
+  if (!name) redirect('/companions');
+
   return (
-    <div>companionSession</div>
+    <main>
+      <article className=" flex rounded-border justify-between p-6 max-md:flex-col ">
+        <div className="flex items-center gap-2">
+          <div className=" flex items-center justify-center size-[72px] rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(subject) }} >
+            <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <div className=" flex items-center gap-2">
+              <p className="font-bold text-2xl ">
+                {name}
+              </p>
+              <div className="subject-badge max-sm:hidden">
+                {subject}
+              </div>
+            </div>
+            <p className="text-lg ">{topic}</p>
+          </div>
+        </div>
+        <div className=" items-start text-2xl max-md:hidden">
+          {duration} minutes
+        </div>
+      </article>
+    </main>
   )
 }
 
